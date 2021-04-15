b2e6cda98c25c3e209c0f7ad27cc2fcc7510ea06
Diana <68639974+viemurr@users.noreply.github.com>
2021-04-15 09:28:57 +0600

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
         if contents.count(chr(i)) != 0:
             val = round(contents.count(chr(i))/len(contents), 3)
             d.update({chr(i): val})
-    for key, index in d.items():
+    for key, index in sorted(d.items(), key=lambda item: item[1],reverse=True):
         print(key + " - " + str(index))
     print(contents)
 
