c86a2119bbf4ffd5486f584390a3273716c349e3
Diana <68639974+viemurr@users.noreply.github.com>
2021-04-15 09:52:57 +0600

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ def main():
         if contents.count(chr(i)) != 0:
             val = round(contents.count(chr(i))/len(contents), 3)
             d.update({chr(i): val})
+    if '\t' in d:
+        d['tab'] = d.pop("\t")
+    if '\n' in d:
+        d['new line'] = d.pop("\n")
+    if ' ' in d:
+        d['whitespace'] = d.pop(" ")
+        
     for key, index in sorted(d.items(), key=lambda item: item[1],reverse=True):
         print(key + " - " + str(index))
     print(contents)
